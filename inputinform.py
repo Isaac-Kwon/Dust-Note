2d74d8c2fd529c51bcc574e3440a17bf197cb51b
Minjae Isaac Kwon <minals3165@gmail.com>
2017-07-13 15:59:37 +0900

    Python3 Printing Update

--- inputinform.py
+++ inputinform.py
@@ -1,58 +1,59 @@
+from __future__ import print_function
 import sys
 import time
 import datetime
-#from __future__ import print_function
+
 
 def goscriptup():
-    print "\033[A                             \033[A \r"
+    print("\033[A                             \033[A \r")
 
 def input_time_rt_update(yearm):
     inputtime = datetime.datetime(2000,01,01,00,00)
-    print inputtime
+    print('%s' %inputtime)
     #
     inputtime = inputtime.replace(year=yearm)###int(raw_input("Year?: ")))
     goscriptup()
     goscriptup()
     #
-    print inputtime
+    print('%s' %inputtime)
     #
     inputtime = inputtime.replace(month=int(raw_input("Month?: ")))
     goscriptup()
     goscriptup()
     #
-    print inputtime
+    print('%s' %inputtime)
     #
     inputtime = inputtime.replace(day=int(raw_input("Day?: ")))
     goscriptup()
     goscriptup()
     #
-    print inputtime
+    print('%s' %inputtime)
     #
     inputtime = inputtime.replace(hour=int(raw_input("Hour?: ")))
     goscriptup()
     goscriptup()
     #
-    print inputtime
+    print('%s' %inputtime)
     #
     inputtime = inputtime.replace(minute=int(raw_input("Minute?: ")))
     goscriptup()
     goscriptup()
     #
-    print inputtime
+    print('%s' %inputtime)
     #
     return inputtime
 
 def printdict(dict):
-    print 'index', '\t',
-    for key in dict.keys():
-        print '|', key, ' \t',
-    print ''
+    print('index\t',end='')
+    for key in dict[dict.keys()[0]].keys():
+        print('|%s\t' %key,end='')
+    print('')
     #
     for key in dict.keys():
-        print key, '\t',
+        print('%s\t' %key,end='')
         for key2 in dict[dict.keys()[0]].keys():
-            print '|', dict[key][key2], '\t\t',
-        print '\n',
+            print('|%s\t' %dict[key][key2],end='')
+        print('')
 
 
 def input_dust(dataclasses, datatypes):
@@ -60,7 +61,7 @@ def input_dust(dataclasses, datatypes):
     for i in range(0,len(dataclasses)):
         datadict[dataclasses[i]] = dict();
         for j in range(0,len(datatypes)):
-            print datatypes[j], 'of', dataclasses[i]
+            print('%s of %s'%(datatypes[j], dataclasses[i]))
             datadict[dataclasses[i]][datatypes[j]] = int(raw_input(" :"))
             goscriptup()
             goscriptup()
@@ -69,5 +70,5 @@ def input_dust(dataclasses, datatypes):
 
 
 if __name__ == "__main__":
-    print ""
+    print("")
     #diction = input_dust(["PM2.5", "PM10"], ["SUM", "SIGMA"])
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import os
 import datetime
+
 
 import inputinform
 
@@ -7,7 +9,7 @@ locations = ["Outside", "Buffer Room", "ALICIA Table", "HIC Table"]
 dataclasses = ["PM2.5", "PM10"]
 
 def goscriptup():
-    print "\033[A                             \033[A"
+    print("\033[A                             \033[A")
 
 
 
@@ -18,15 +20,13 @@ datatable = dict()
 for i in range(0,len(dataclasses)):
     datatable.update({dataclasses[i]:{"Value" : 0.0, "Sigma" :0.0}})
 
-#print datatable
-
 ########################################################################
 print('===================================')
 print('Dust Monitoring Data Sending Script')
 
 #timeseq = ['Year', 'Month', 'Day', 'Hour', 'Minute']
 #ntimeseq = len(timeseq)
-print 'date?' '\n' ' (blank if realtime)'
+print('date? \n(blank if realtime)')
 
 input_time1 = raw_input('Year: ')
 if input_time1 == '':
@@ -35,21 +35,21 @@ else:
     input_time = inputinform.input_time_rt_update(int(input_time1))
 
 goscriptup()
-print "Time : ", input_time
+print("Time : %s" %input_time)
 
 ########################################################################
-print ''
+print('')
 print('Location?')
 for i in range(0,len(locations)):
-    print i+1, ': ' ,locations[i]
+    print("%d: %s" %(i+1,locations[i]))
 
 loc2 = raw_input('')
 loc = int(loc2)-1
 
 goscriptup()
-print('Location: ' + locations[int(loc)])
+print('Location: %s' %locations[int(loc)])
 
-print ''
+print('')
 
 dustdata = inputinform.input_dust(["PM2.5", "PM10"], ["SUM", "SIGMA"])
 

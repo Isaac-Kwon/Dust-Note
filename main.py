53a02c757e2f52a9be98a9490a7f478da0e907b1
Minjae Isaac Kwon <minals3165@gmail.com>
2017-07-17 18:40:32 +0900

    Input parameter help update

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ import google_upload
 #Input Argument
 #python main.py timestamp(0 or YYYY.MM.DD_HH:MM) locationNo 0.3um 0.5um 5um
 
+#or
+
+#python main.py --help
+#or
+#python main.py -h
+
 #sys.argv[0] #TimeStamp or Realtime Parameter
 #sys.argv[1] #first Value
 #sys.argv[2] #second Value
@@ -17,7 +23,22 @@ import google_upload
 with open('cfg.json', 'r') as cfgfile:
     cfgdata = json.load(cfgfile)
 
-
+if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+    print("HELP DISPLAY for Input Parameter")
+    print("python main.py timestamp(0 or YYYY/MM/DD-HH:MM) ", end='')
+    if len(cfgdata["Locations"])!=1:
+        print('LocationNumber ', end='')
+    for i in range(0,len(cfgdata['Type1'])):
+        for j in range(0,len(cfgdata['Type2'])):
+            print(cfgdata['Type2'][j]+'of'+cfgdata['Type1'][i], end=' ')
+    print('')
+    if len(cfgdata["Locations"])!=1:
+        print('\nlocation numbers')
+        locationind = 1
+        for location in list(cfgdata["Locations"]):
+            print("%d: %s" %(locationind, location))
+            locationind = locationind + 1
+    exit()
 
 ########################################################################
 

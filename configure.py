e0907302764a35222572d217a878fa14aa1c9f65
Minjae Isaac Kwon <minals3165@gmail.com>
2017-07-17 18:39:29 +0900

    Information Setup with Configuration

--- configure.py
+++ configure.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from builtins import input
+import sys,os
 import json
 
 #configuration main function's destinations
@@ -9,23 +10,51 @@ import json
 #dataclasses : size of particles
 #datatypes : SUM, SIGMA or etc
 #Locations
+def initialize():
+    if not os.path.exists("cfg.json"):
+        with open('cfg.json','w') as genfile:
+            genjson = {"Locations": "none", "Type1": "none", "Type2": "none", "SheetFile Name": "none"}
+            json.dump(genjson,genfile, ensure_ascii=False)
 
-def configtypes(init_name,suffix):
+def configtypes(init_name,suffix,original):
     returnarray = list();
-    loop_number = int(input('number of %s: ' %init_name))
+    print('Original Configuration data of %s' %(init_name))
+    print(original)
+    loop_number_str = input('number of %s: ' %init_name)
+    if loop_number_str == '':
+        return original
+    else:
+        loop_number = int(loop_number_str)
     for i in range(0,loop_number):
         returnarray.append(input('No.%d types? ' %(i+1))+suffix)
     return returnarray
 
+def config_one(question, original):
+    print('Original Configuration data of %s' %(question))
+    print(original)
+    temp_name = input(question+'?')
+    if temp_name == '':
+        return original
+    else:
+        return temp_name
+
 if __name__ == "__main__":
+    initialize()
+    print("\n========================================")
+    print("Dust Monitoring Script Configure")
+    print("You can set Monitoring Location, ParticleSize, CollectingInformation, GoogleSpreadsheet\'s Filename\n")
+    print("for all of Question, it will show original value of configure file, you can leave it at that or overwrite each configuration")
+    print("just press RETURN to leave, put information to overwrite\n")
+    with open('cfg.json', 'r') as infile:
+        originaldic = json.load(infile)
     outputjson = dict()
-    outputjson["Locations"] = configtypes('locations','')
+    outputjson["Locations"] = configtypes('locations','', originaldic["Locations"])
     print('')
-    outputjson["Type1"] = configtypes('particle types', 'um')
+    outputjson["Type1"] = configtypes('particle types', 'um', originaldic["Type1"])
     print('')
-    outputjson["Type2"] = configtypes('data types', '')
+    outputjson["Type2"] = configtypes('data types', '', originaldic["Type2"])
     print('')
-    outputjson["SheetFile Name"] = input('spreadsheet file\'s name?')
+    outputjson["SheetFile Name"] = config_one('SpreadSheet Name', originaldic["SheetFile Name"])
     print('')
     jsonString = json.dumps(outputjson, indent=4)
     print('')
